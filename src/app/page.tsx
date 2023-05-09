de26a51a401fad7742e4b38e386a1ca76a771697
gregoryMD <gdiehl265@gmail.com>
2023-05-09 14:43:35 -0400

    main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 import Image from "next/image";
-import Header from "../components/Header";
-import List from "@/components/List";
-import Details from "@/components/Details";
-import Footer from "@/components/Footer";
+import MaterialsCatalog from "@/components/MaterialsCatalog";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Materials Demo </h1>
-      <div>
-        <Header />
-        <List />
-        <Details />
-        <Footer />
-      </div>
+      <h1>Materials Catalog </h1>
+      <MaterialsCatalog />
     </main>
   );
 }
---
+++ src/components/MaterialsCatalog.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import List from "@/components/List";
+import Details from "@/components/Details";
+import Footer from "@/components/Footer";
+
+export default function MaterialCatalog() {
+  return (
+    <div>
+      <Header />
+      <List />
+      <Details />
+      <Footer />
+    </div>
+  );
+}
